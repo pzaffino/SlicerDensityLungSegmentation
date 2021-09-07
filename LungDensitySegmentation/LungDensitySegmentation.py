d60986fdd02eac571d187ed3cca5d0e5c0992f63
Paolo Zaffino <p.zaffino@yahoo.it>
2021-09-07 10:23:31 +0200

    removev dummy test, move libraries installation, add information about timing

--- LungDensitySegmentation/LungDensitySegmentation.py
+++ LungDensitySegmentation/LungDensitySegmentation.py
@@ -10,18 +10,6 @@ import SimpleITK as sitk
 import sitkUtils
 import scipy.ndimage
 
-try:
-  import joblib
-except ModuleNotFoundError:
-  slicer.util.pip_install("joblib")
-  import joblib
-
-try:
-  import sklearn
-except ModuleNotFoundError:
-  slicer.util.pip_install("scikit-learn")
-  import sklearn
-
 #
 # LungDensitySegmentation
 #
@@ -119,7 +107,7 @@ class LungDensitySegmentationWidget(ScriptedLoadableModuleWidget):
     #
     # Apply Button
     #
-    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton = qt.QPushButton("Apply (it can takes a few minutes)")
     self.applyButton.toolTip = "Run the algorithm."
     self.applyButton.enabled = False
     parametersFormLayout.addRow(self.applyButton)
@@ -282,6 +270,19 @@ class LungDensitySegmentationLogic(ScriptedLoadableModuleLogic):
     Run intensity labeling
     """
 
+    # Import the required libraries
+    try:
+      import joblib
+    except ModuleNotFoundError:
+      slicer.util.pip_install("joblib")
+      import joblib
+
+    try:
+      import sklearn
+    except ModuleNotFoundError:
+      slicer.util.pip_install("scikit-learn")
+      import sklearn
+
     # Get sitk/numpy images from Slicer
     CT_sitk = sitk.Cast(sitkUtils.PullVolumeFromSlicer(CTVolumeName), sitk.sitkFloat32)
     CT_np = sitk.GetArrayFromImage(CT_sitk)
@@ -340,50 +341,3 @@ class LungDensitySegmentationLogic(ScriptedLoadableModuleLogic):
     averagedOutputVolume = sitkUtils.PushVolumeToSlicer(filtered_label_sitk, averagedOutputVolume)
     setSliceViewerLayers(background=outputVolume)
 
-
-class LungDensitySegmentationTest(ScriptedLoadableModuleTest):
-  """
-  This is the test case for your scripted module.
-  Uses ScriptedLoadableModuleTest base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
-
-  def setUp(self):
-    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
-    """
-    slicer.mrmlScene.Clear(0)
-
-  def runTest(self):
-    """Run as few or as many tests as needed here.
-    """
-    self.setUp()
-    self.test_LungDensitySegmentation1()
-
-  def test_LungDensitySegmentation1(self):
-    """ Ideally you should have several levels of tests.  At the lowest level
-    tests should exercise the functionality of the logic with different inputs
-    (both valid and invalid).  At higher levels your tests should emulate the
-    way the user would interact with your code and confirm that it still works
-    the way you intended.
-    One of the most important features of the tests is that it should alert other
-    developers when their changes will have an impact on the behavior of your
-    module.  For example, if a developer removes a feature that you depend on,
-    your test should break so they know that the feature is needed.
-    """
-
-    self.delayDisplay("Starting the test")
-    #
-    # first, get some data
-    #
-    import SampleData
-    SampleData.downloadFromURL(
-      nodeNames='FA',
-      fileNames='FA.nrrd',
-      uris='http://slicer.kitware.com/midas3/download?items=5767',
-      checksums='SHA256:12d17fba4f2e1f1a843f0757366f28c3f3e1a8bb38836f0de2a32bb1cd476560')
-    self.delayDisplay('Finished with download and loading')
-
-    volumeNode = slicer.util.getNode(pattern="FA")
-    logic = LungDensitySegmentationLogic()
-    self.assertIsNotNone( logic.hasImageData(volumeNode) )
-    self.delayDisplay('Test passed!')
